8a6c2204da0a8529d20769086da3064b6ba1a04b
Cooper Lawson <CSLawson@lcs.on.ca>
2023-04-12 11:36:15 -0400

    Initial Commit

---
+++ SearchList/SearchListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchListApp.swift
+//  SearchList
+//
+//  Created by Cooper Lawson on 2023-04-12.
+//
+
+import SwiftUI
+
+@main
+struct SearchListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
